0c2f6c3a674ee008039fa1db9acee781f750ab19
SuicinivArievilo <vinicius_og@outlook.com>
2022-05-13 00:10:08 -0300

    Primeiro teste
    Mensagem de bem vindo na homepage

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
 import '../styles/globals.css'
+import '../styles/homepage.css';  
+import 'bootstrap/dist/css/bootstrap.min.css';
+import tittle from '../src/components/text-content/text-content.js';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
---
+++ src/components/text-content/text-content.js
@@ -0,0 +1,11 @@
+
+export default function hpTittle ({children, ...props}) {
+    return (
+
+        <hpTittle>
+           <h1 {...props}>{children} </h1> 
+        </hpTittle>
+
+    )
+}
+
